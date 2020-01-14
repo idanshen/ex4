5fec6c7fa8f34153460058d055fb6d83843f353f
idan shenfeld <idanshen@campus.technion.ac.il>
2020-01-14 13:14:58 +0100

    change main for testing

--- main.cpp
+++ main.cpp
@@ -6,11 +6,13 @@ int main() {
     tmp.AddServer(1,1);
     tmp.AddServer(2,2);
     tmp.AddServer(3,3);
+    tmp.AddServer(3,5);
     tmp.AddServer(4,4);
 
     tmp.SetTraffic(1,8);
     tmp.SetTraffic(2,4);
     tmp.SetTraffic(3,1);
+    tmp.SetTraffic(5,7);
     tmp.SetTraffic(4,2);
 
     tmp.RemoveServer(1);
